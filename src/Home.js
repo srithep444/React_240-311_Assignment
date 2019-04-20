22ec2aea4bc3497513f0f0258db5bc638493fc8b
srithep444 <srithep555@gmail.com>
2019-04-21 03:24:10 +0700

    จัดโค้ดและไฟล์โดยแบ่งเป็น components

--- src/Home.js
+++ src/Home.js
@@ -1,30 +1,20 @@
 import React, { Component } from 'react';
 import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
 import firebase from 'firebase';
-import config from './firebase/config';
+import config from './components/firebase/config';
+import uiConfig from './components/firebase/GoogleLogin';
 import './Home.css';
 
 firebase.initializeApp(config);
 
-// Configure FirebaseUI.
-const uiConfig = {
-  // Popup signin flow rather than redirect flow.
-  signInFlow: 'popup',
-  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
-  signInSuccessUrl: '/signedIn',
-  // We will display Google and Facebook as auth providers.
-  signInOptions: [
-    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-    firebase.auth.FacebookAuthProvider.PROVIDER_ID
-  ]
-};
-
 class Home extends Component {
   render() {
     return (
-      <div>
-        <h1>Assignmant App</h1>
-        <p>กรุณา Log in ก่อนเข้าใช้</p>
+      <div className = "container-fluid">
+        <div className = "text-center">
+          <h1>Assignmant Web</h1>
+          <p>กรุณา Log in ก่อนเข้าใช้</p>
+        </div>
         <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()}/>
       </div>
     );
--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import Home from './Home';
 import * as serviceWorker from './serviceWorker';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(<Home />, document.getElementById('root'));
 
