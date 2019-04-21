d4943d0f8310310bbaa8847807ba9c6142083c1e
srithep444 <srithep555@gmail.com>
2019-04-22 06:54:56 +0700

    เพิ้มหน้า 404 Not Found

---
+++ src/pages/NotFound.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const NotFound = () => {
+    return(
+      <div className= "container col-md-8 text-center">
+        <h1 className="mt-5" style={{fontSize: 120}}>404</h1>
+        <h1 className="mt-1">Not Found</h1>
+        <p className="title mb-5">ขออภัยไม่พบหน้าที่คุณค้นหากรุณาค้นหาหน้าที่ถูกต้องอีกครั้ง</p>
+      </div>
+    )
+}
+
+export default NotFound;
